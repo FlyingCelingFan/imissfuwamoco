edf5f319acd75ab1cd7ca55a1abf7c1ac446d012
NeedlesP <nijicunny@gmail.com>
2023-08-11 08:03:53 -1000

    change source url

--- components/page_meta.js
+++ components/page_meta.js
@@ -35,7 +35,7 @@ export function CommonFooter(props) {
         <small>
             {lang.formatString(
                 lang.CommonMetadata.FooterText,
-                <a href="https://github.com/cyberiumshadow/imissshiori.com">{lang.CommonMetadata.FooterSourceLink}</a>
+                <a href="https://github.com/NeedlesP/fuwamoco">{lang.CommonMetadata.FooterSourceLink}</a>
             )}
         </small>
     </footer>
